45fd66154cd0418ac7c7a05fe0c2b73b3381d393
gabrielsiron <gabrielsiron.menezes@gmail.com>
2023-10-14 23:49:27 -0300

    add suporte para realização de operações unárias

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-from proposition import Proposition, CompostProposition
+from proposition import Proposition, CompoundProposition
 
 p = Proposition(text='p', value=True)
 q = Proposition(text='q', value=True)
@@ -7,14 +7,12 @@ q = Proposition(text='q', value=True)
 print(f'p é {p.value}')
 print(f'q é {q.value}')
 
-expression = CompostProposition()
+expression = CompoundProposition()
 expression.add(p)
-expression.add(p.__mul__)
-expression.add(q)
-# expression2 = CompostProposition()
-# expression2.add(p)
-# expression2.add(p.__mul__)
-# expression2.add(q)
-# expression.add(expression2)
+expression.add(p.__add__)
+negation = CompoundProposition()
+negation.add(p.__invert__)
+negation.add(p)
+expression.add(negation)
 print(expression)
 print(expression.calculate_value(debug=True))
